87eff65c097305dc27451f99a1da853117064007
Lara <numbersandlettersandallthat@gmail.com>
2015-11-08 00:19:03 -0500

    Basic functionality for MC implementation

---
+++ concurrency-CS343/a4/q2tallyVotes.cc
@@ -0,0 +1,220 @@
+#include <iostream>
+using namespace std;
+
+#include "q2tallyVotes.h"
+#include "MPRNG.h"
+
+MPRNG *mprng;                                                                   //randomizer for yield values
+
+TallyVotes::TallyVotes(unsigned int group, Printer &printer) :
+    group(group), printer(printer){}
+
+Voter::Voter(unsigned int id, TallyVotes &voteTallier, Printer &printer ) :
+    id(id), voteTallier(voteTallier), printer(printer) {};
+
+
+#if defined( IMPLTYPE_MC )
+TallyVotes::Tour TallyVotes::vote(unsigned int id, TallyVotes::Tour ballot){
+    mlk.acquire();
+    Tour ret;
+    if(ballot == Picture){
+        picCount += 1;
+    } else {
+        statCount += 1;
+    }
+    if(groupCount < (int)group){
+        printer.print(id, Voter::Block, groupCount++);
+        clk.wait(mlk);
+        printer.print(id, Voter::Unblock, groupCount--);
+    } else {
+        ret = (picCount > statCount) ? Picture : Statue;
+        clk.broadcast();
+        printer.print(id, Voter::Complete);
+        picCount = statCount = 0;
+    }
+    if(groupCount == 0){
+        mlk.release();
+    }
+    return ret;
+}
+
+void Voter::main(){
+    printer.print(id, Start);
+    yield((*mprng)(0, 19));
+    TallyVotes::Tour ballot = static_cast<TallyVotes::Tour>( (*mprng)(0,1));
+    printer.print(id, Vote, ballot);
+    TallyVotes::Tour res = voteTallier.vote(id, ballot);
+    printer.print(id, Finished, res);
+}
+#endif
+
+/*#if defined( IMPLTYPE_BAR )
+Tour TallyVotes::vote(unsigned int id, Tour ballot){
+}
+void Voter::main(){
+}
+#endif
+
+#if defined( IMPLTYPE_SEM )
+TallyVotes::Tour TallyVotes::vote(unsigned int id, Tour ballot){
+}
+void Voter::main(){
+}
+#endif*/
+
+
+Printer::Printer(unsigned int voters) : voters(voters){
+    data = new VoterData*[voters];
+
+
+    for(unsigned int i = 0; i < voters; i++){
+        cout << "Voter" << i << "\t";
+    }
+    cout << endl;
+
+    for(unsigned int i = 0; i < voters; i++){
+        cout << "======= ";
+    }
+    cout << endl;
+
+    for(int i = 0; i < (int)voters; i++){
+        data[i] = NULL;
+    }
+
+}
+
+Printer::~Printer() {
+    cout << "=================" << endl;
+    cout << "All tours started" << endl;
+    delete data;
+}
+
+void Printer::print(unsigned int id, Voter::States state) {
+    if(data[id]){
+        flush();
+    }
+    data[id] = new VoterData();
+    data[id]->state = state;
+
+}
+void Printer::print(unsigned int id, Voter::States state, TallyVotes::Tour vote) {
+    //Special case for task finishing
+    if(state == Voter::Finished){
+        flush();
+
+        for(unsigned int i = 0; i < voters; i++){
+            if(i == id){
+                cout << state << " " << vote;
+            } else {
+                cout << "...";
+            }
+            cout << "\t";
+        }
+        return;
+    }
+
+
+    if(data[id]){
+        flush();
+    }
+
+    data[id] = new VoterData();
+    data[id]->state = state;
+    data[id]->vote = vote;
+}
+void Printer::print(unsigned int id, Voter::States state, unsigned int numblocked) {
+    if(data[id]){
+        flush();
+    }
+
+    data[id] = new VoterData();
+    data[id]->state = state;
+    data[id]->value = numblocked;
+}
+
+void Printer::flush() {
+    for(int i = 0; i < (int)voters; i++){
+        if(data[i]){
+            cout << (char)data[i]->state << " ";
+            if(data[i]->value){
+                cout << data[i]->value;
+            }
+
+            if(data[i]->vote == TallyVotes::Picture){
+                cout << "p";
+            } else {
+                cout << "s";
+            }
+        }
+
+        cout << "\t";
+    }
+    cout << endl;
+
+    for(int i = 0; i < (int)voters; i++){
+        delete data[i];
+        data[i] = NULL;
+    }
+}
+
+
+void uMain::main() {
+    int tourSize = 6, groupSize = 3, seed = getpid();
+
+    switch(argc){
+        case 1:
+            try{
+                seed = stoi(argv[3]);
+            } catch(...){
+                cerr << "Error: Seed must be an integer." << endl;
+                exit(-1);
+            }//try
+
+            if(seed <= 0){
+                cerr << "Error: Seed must be greater than 0." << endl;
+                exit(-1);
+            }//if
+        case 3:
+            try{
+                groupSize = stoi(argv[2]);
+            } catch(...){
+                cerr << "Error: Group size must be an integer." << endl;
+                exit(-1);
+            }//try
+
+            if(groupSize <= 0){
+                cerr << "Error: Group size must be greater than 0." << endl;
+                exit(-1);
+            }//if
+
+            if(groupSize % 2 == 0){
+                cerr << "Error: Group size must be odd." << endl;
+                exit(-1);
+            }//if
+        case 2:
+            try{
+                tourSize = stoi(argv[1]);
+            } catch(...){
+                cerr << "Error: Tour size must be an integer." << endl;
+                exit(-1);
+            }//try
+
+            if(tourSize % groupSize != 0){
+                cerr << "Error: Tour size must be divisible by group size." << endl;
+                exit(-1);
+            }//if
+    }//switch
+
+    mprng = new MPRNG(seed);
+    Printer printer(tourSize);
+    TallyVotes tallier(groupSize, printer);
+    Voter *voters[tourSize];
+
+    for(int i = 0; i < tourSize; i++){
+        voters[i] = new Voter((unsigned int)i, tallier, printer);
+    }
+
+    for(int i = 0; i < tourSize; i++){
+        delete voters[i];
+    }
+}
---
+++ concurrency-CS343/a4/q2tallyVotes.h
@@ -0,0 +1,54 @@
+_Monitor Printer;
+
+#if defined( IMPLTYPE_MC )
+class TallyVotes {
+    uOwnerLock mlk;
+    uCondLock clk;
+    int picCount = 0;
+    int statCount = 0;
+    int groupCount = 0;
+#elif defined( IMPLTYPE_BAR )
+_Cormonitor TallyVotes : public uBarrier {
+#elif defined( IMPLTYPE_SEM )
+class TallyVotes {
+#else
+    #error unsupported voter type
+#endif
+    unsigned int group;
+    Printer &printer;
+  public:
+    TallyVotes( unsigned int group, Printer &printer );
+    enum Tour { Picture, Statue };
+    Tour vote( unsigned int id, Tour ballot );
+};
+
+
+_Task Voter {
+  unsigned int id;
+  TallyVotes & voteTallier;
+  Printer &printer;
+
+  void main();
+  public:
+    enum States { Start = 'S', Vote = 'V', Block = 'B', Unblock = 'U',
+                   Complete = 'C', Finished = 'F' };
+    Voter( unsigned int id, TallyVotes &voteTallier, Printer &printer );
+};
+
+struct VoterData{
+  Voter::States state;
+  unsigned int value;
+  TallyVotes::Tour vote;
+};
+
+_Monitor Printer {
+  unsigned int voters;
+  VoterData **data;
+  void flush();
+  public:
+    Printer( unsigned int voters );
+    ~Printer();
+    void print( unsigned int id, Voter::States state );
+    void print( unsigned int id, Voter::States state, TallyVotes::Tour vote );
+    void print( unsigned int id, Voter::States state, unsigned int numBlocked );
+};
