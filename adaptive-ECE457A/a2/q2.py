69d0cbceb70a17a530074730d87cef96581ed61b
Lara Janecka <numbersandlettersandallthat@gmail.com>
2016-07-07 13:32:18 -0400

    Adaptive a2 q2 basic

---
+++ adaptive-ECE457A/a2/q2.py
@@ -0,0 +1,178 @@
+from copy import copy, deepcopy
+from random import randint, shuffle
+
+BOARD_SIZE 	= 4
+DEPTH_LIMIT	= 3
+UP 			= "up"
+DOWN 		= "down"
+LEFT 		= "left"
+RIGHT 		= "right"
+UPLEFT 		= "upleft"
+UPRIGHT 	= "upright"
+DOWNLEFT 	= "downleft"
+DOWNRIGHT 	= "downright"
+RATIONAL 	= "rational agent"
+RANDOM 		= "random agent"
+
+
+class Cell:
+	def __init__(self, stones=0, user=None):
+		self.stones = stones
+		self.user = user
+	def __str__(self):
+		if self.user == RATIONAL:  return "A:" + str(self.stones)
+		elif self.user == RANDOM:  return "B:" + str(self.stones)
+		else: return "N:" + str(self.stones)
+	def isAvailable(self, user):
+		return self.stones == 0 or self.user == user
+	def empty(self):
+		self.stones = 0
+		self.user = ""
+
+
+def moveX(direction, x, value):
+	if("left" in direction): return x - value
+	if("right" in direction): return x + value
+	return x
+
+def moveY(direction, y, value):
+	if("up" in direction): return y - value
+	if("down" in direction): return y + value
+	return y
+
+def move(user, direction, board, x, y):
+	stonesLeft = board[y][x].stones
+	temp = deepcopy(board)
+	newX1 = moveX(direction, x, 1)
+	newY1 = moveY(direction, y, 1)
+	if(0 <= newX1 < BOARD_SIZE and 0 <= newY1 < BOARD_SIZE and temp[newY1][newX1].isAvailable(user)):
+		temp[y][x].empty()
+		temp[newY1][newX1].stones += 1
+		temp[newY1][newX1].user = user
+		stonesLeft -= 1
+	else: return None
+
+	newX2 = moveX(direction, x, 2)
+	newY2 = moveY(direction, y, 2)
+	if(0 <= newX2 < BOARD_SIZE and 0 <= newY2 < BOARD_SIZE and temp[newY2][newX2].isAvailable(user)):
+		toAdd = min(stonesLeft, 2)
+		temp[newY2][newX2].stones += toAdd
+		temp[newY2][newX2].user = user
+		stonesLeft -= toAdd
+	else:
+		temp[newY1][newX1].stones += stonesLeft
+		return temp
+
+	newX3 = moveX(direction, x, 3)
+	newY3 = moveY(direction, y, 3)
+	if(0 <= newX3 < BOARD_SIZE and 0 <= newY3 < BOARD_SIZE and temp[newY3][newX3].isAvailable(user)):
+		temp[newY3][newX3].stones += stonesLeft
+		temp[newY3][newX3].user = user
+	else:
+		temp[newY2][newX2].stones += stonesLeft
+
+	return temp
+
+def getChildren(board, user):
+	moves = []
+	for y in range(0,BOARD_SIZE):
+		for x in range(0,BOARD_SIZE):
+			if(board[y][x].user != user): continue
+			moves.append(move(user, LEFT, board, x, y))
+			moves.append(move(user, UP, board, x, y))
+			moves.append(move(user, RIGHT, board, x, y))
+			moves.append(move(user, DOWN, board, x, y))
+			moves.append(move(user, UPLEFT, board, x, y))
+			moves.append(move(user, UPRIGHT, board, x, y))
+			moves.append(move(user, DOWNLEFT, board, x, y))
+			moves.append(move(user, DOWNRIGHT, board, x, y))
+
+	ret = [x for x in moves if x is not None]
+
+	shuffle(ret)
+	return ret
+
+
+def rationalMove(board, isRational, alpha, beta, depth):
+	rationalChildren = getChildren(board, RATIONAL)
+	randomChildren = getChildren(board, RANDOM)
+
+	if(isRational and (len(randomChildren) == 0 or depth == DEPTH_LIMIT)): 
+		return len(rationalChildren) - len(randomChildren)
+	if(not isRational and (len(rationalChildren) == 0 or depth == DEPTH_LIMIT)): 
+		return len(rationalChildren) - len(randomChildren)
+
+	children = rationalChildren if isRational else randomChildren
+
+	for child in children:
+		if(isRational):
+			childVal = rationalMove(child, not isRational, alpha, beta, depth + 1)
+			if alpha == None: alpha = childVal
+			alpha = max(alpha, childVal) 
+			if(beta != None and childVal > beta): 
+				return alpha
+		else:
+			childVal = rationalMove(child, not isRational, alpha, beta, depth + 1)
+			if beta == None: beta = childVal
+			beta = min(beta, childVal)
+			if(alpha != None and childVal < alpha): 
+				return beta
+	return alpha if isRational else beta
+
+def randomAgent(board):
+	children = getChildren(board, RANDOM)
+	return children[randint(0, len(children) - 1)]
+
+def rationalAgent(board):
+	score = None
+	bestMove = None
+	for child in getChildren(board, RATIONAL):
+		childVal = rationalMove(child, False, None, None, 0)
+		if(score == None or childVal < score):
+			score = childVal
+			bestMove = child
+
+	return bestMove
+
+def printBoard(board):
+	print("Board")
+	for y in range(0,BOARD_SIZE):
+		for x in range(0,BOARD_SIZE):
+			print(board[y][x], " ",  end="")
+		print("")
+
+
+if __name__ == '__main__':
+	board = []
+
+	for y in range(0,BOARD_SIZE):
+		row = []
+		for x in range(0,BOARD_SIZE):
+			row.append(Cell())
+		board.append(row)
+
+	board[0][0].user = RATIONAL
+	board[0][0].stones = 10
+	board[BOARD_SIZE-1][BOARD_SIZE-1].user = RANDOM
+	board[BOARD_SIZE-1][BOARD_SIZE-1].stones = 10
+
+	for i in range(0,10):
+		while True:
+			if(len(getChildren(board, RATIONAL)) > 0):
+				print("Rational player turn")
+				board = rationalAgent(board)
+				printBoard(board)
+			else:
+				print("Random player wins")
+				printBoard(board)
+				break
+			if(len(getChildren(board, RANDOM)) > 0):
+				print("Random player turn")
+				board = randomAgent(board)
+				printBoard(board)
+			else:
+				print("Rational player wins")
+				printBoard(board)
+				break
+
+
