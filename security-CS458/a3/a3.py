147d23ced30f1d862502ebaa56416730178003e2
Lara Janecka <numbersandlettersandallthat@gmail.com>
2016-07-25 15:06:20 -0400

    security decrypt first byte working

--- security-CS458/a3/a3.py
+++ security-CS458/a3/a3.py
@@ -4,69 +4,55 @@ import requests
 import base64
 
 URL = 'http://localhost:4555'
-
+padding = bytearray([15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 16])
 
 def test(cookie):
-	encode = base64.b64encode(cookie)
+    encode = base64.b64encode(cookie)
 
-	print encode
-	r = requests.get(URL, cookies={'user': encode})
+    r = requests.get(URL, cookies={'user': encode})
 
-	return r.status_code != 500
-
-def getPlain(padLen, i):	
-	if i == 0:
-		return padLen
-
-	return i
-
-def printBytes(title, data):	
-	print title, ''.join('{:02x}'.format(x) for x in data)
+    return r.status_code != 500
 
 def decryptByte(cookie):
-	original = cookie[:16]
-	iv = bytearray(16) #this will be backwards
+    original = bytearray(cookie[:16])
+    cookie = bytearray(cookie[16:])
+    iv = bytearray(16)
 
-	# printBytes("Initial cookie", cookie)
+    for i in range(15, -1, -1):
+        #cycle through to get next value
+        while not test(iv + cookie):
+            iv[i] += 1
+            if iv[i] > 255:
+                print "This should not have happened"
 
-	for place in range(16):
-		print "START:", place
-		prefix = bytearray(16) #this will be backwards
-
-		# printBytes("iv", iv)
-		for i in range(place):
-			# print place, i, getPlain(place, i)
-			prefix[15-i] = iv[15-i] ^ getPlain(place, i)
-
-
-		# printBytes("prefix", prefix)
-
-		for i in range(256):
-			prefix[15-place] = i
-
-			if(test(prefix + cookie)):
-				print "chosen val", i
-				break
-
-		iv[15-place] = prefix[15-place] ^ getPlain(place, place)
+        #undo padding on last byte
+        iv[15] = iv[15] ^ (16 - i)
+        #apply new padding on last byte
+        iv[15] = iv[15] ^ (16 - i + 1)
 
 
-	plaintext = [0] * 16
-	for i in range(16):
-		plaintext[i] = iv[i] ^ cookie[i]
+    intermediate = bytearray(16)
+    for i in range(16):
+        intermediate[i] = iv[i] ^ padding[i]
 
-	return plaintext
 
+    plaintext = bytearray(16)
+    for i in range(16):
+        plaintext[i] = intermediate[i] ^ original[i]
+
+    print ''.join(chr(x) for x in plaintext)
+
+    return plaintext
 
 
 
 
 def main():
-	dummy = "k0UtzN9gwc1qfe2CMYn3rESGL6mJwyG8Z+ztkAJJBVM="
-	cookie =  base64.b64decode(dummy)
+    dummy = "k0UtzN9gwc1qfe2CMYn3rESGL6mJwyG8Z+ztkAJJBVM="
+    cookie =  base64.b64decode(dummy)
 
-	plaintext = decryptByte(cookie)
-	print (bytearray(plaintext))
+    plaintext = decryptByte(cookie)
+    # print (bytearray(plaintext))
 
 if __name__ == '__main__':
     main()
